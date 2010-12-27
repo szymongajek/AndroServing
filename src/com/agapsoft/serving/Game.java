d94eb5d03aff9bb460ba6d3026db7b090565fb70
sz <sz@193.200.118.84>
2010-12-27 21:00:29 +0100

    test

--- src/com/agapsoft/serving/Game.java
+++ src/com/agapsoft/serving/Game.java
@@ -7,7 +7,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-
+/**
+ * 
+ * 
+ */
 public class Game extends Activity {
 	
 	private GameLevel gameLevel;
